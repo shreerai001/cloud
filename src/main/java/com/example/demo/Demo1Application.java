91344ac3f0910334b05677d115d2d341a31220c8
shreerai001 <shreerai1@outlook.com>
2022-01-07 22:32:10 +0545

    dummy controller added

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,9 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Demo1Application {
+
+
+    @RestController
+    class HelloworldController {
+        @GetMapping("/")
+        String hello() {
+            return "Hello " + "!";
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
